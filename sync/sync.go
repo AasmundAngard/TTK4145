5441b9f5af74bdff8613c7f8d4190f454cc69428
krosten <Krisost@outlook.com>
2026-02-18 11:26:30 +0100

    idk

--- sync/sync.go
+++ sync/sync.go
@@ -1,1 +1,26 @@
 package sync
+
+import (
+	"root/elevio"
+)
+
+type msgtosync struct {
+	hallCalls hallCalls
+	cabCalls  cabCalls
+}
+
+type hallBools [config.NumFloors][2]bool
+type cabBools [config.NumFloors]bool
+
+type msgtomain struct {
+	hallBools hallBools
+	cabBools  [3]cabBools
+	elevatorStates [3]ElevState
+}
+
+func Sync(hardwareCalls chan msgtosync, syncedData chan msgtomain) {
+	for{
+		select {
+		}
+	}
+}

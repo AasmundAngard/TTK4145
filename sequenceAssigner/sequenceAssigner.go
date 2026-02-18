02da9636f9cb1d33d7be75994de2981fdde25eea
bmmazzet <bmmazzet@stud.ntnu.no>
2026-02-18 11:17:55 +0100

    working on sequenceAssigner

--- sequenceAssigner/sequenceAssigner.go
+++ sequenceAssigner/sequenceAssigner.go
@@ -1,1 +1,32 @@
 package sequenceAssigner
+
+import (
+	"root/elevio"
+)
+
+type ElevBehaviour int
+
+const (
+	Idle ElevBehaviour = iota
+	DoorOpen
+	Moving
+)
+
+type DirnBehaviourPair struct {
+	dirn 		elevio.MotorDirection
+	behaviour	ElevBehaviour
+}
+
+func requestsAbove(requests []Call, currentFloor int) bool {
+	for 
+}
+
+// Returns next direction and behaviour based on call-requests and current direction and floor
+func assign(requests []Call, dir elevio.MotorDirection, currentFloor int) {
+	switch dir {
+	case elevio.MD_Up:
+
+	default:
+		return
+	}
+}

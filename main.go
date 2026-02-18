09ea99d60a42e4a15f5ee4b6464fb0694c615d40
bmmazzet <bmmazzet@stud.ntnu.no>
2026-02-18 12:09:27 +0100

    Changes structs

--- main.go
+++ main.go
@@ -20,10 +20,15 @@ type Call struct {
 type ElevState struct {
 	behaviour   Behaviour
 	floor       int
-	direction   MotorDirection
-	cabRequests []bool
-	timeStamp   time //?
+	direction   elevio.MotorDirection
 }
+
+type CallList struct {
+	hallCalls	[]Call // 2n elementer to Call-objekt for hver etasje, en for opp og en for ned: 
+	// [Call_etg1_opp, Call_etg1_ned, Call_etg2_opp, Call_etg2_ned, ..., Call_etgn_opp, Call_etgn_ned]
+	cabCalls	[]Call // n elementer, ett Call-objekt for hver etasje
+}
+
 
 func main() {
 	fmt.Println("Hello TTK4145")

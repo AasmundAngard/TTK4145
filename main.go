a25b8d1cde1e3971b2b84de63d01ba4306af15a4
AasmundAngard <wefling@live.no>
2026-02-18 09:41:48 +0100

    minor: few variables

--- main.go
+++ main.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 )
 
+type Behaviour int
+
+const (
+	idle     Behaviour = 0
+	moving             = 1
+	doorOpen           = 2
+)
+
 type Call struct {
 	needService bool
 	timeStamp   time
+}
+
+type ElevState struct {
+	behaviour   Behaviour
+	floor       int
+	direction   MotorDirection
+	cabRequests []bool
+	timeStamp   time //?
 }
 
 func main() {

a874930073e7e836888510cd97a83a64ae3da47c
krosten <Krisost@outlook.com>
2026-02-18 12:03:42 +0100

    added

--- sync/sync.go
+++ sync/sync.go
@@ -1,1 +1,41 @@
 package sync
+
+import(
+	root/config
+)
+
+type Call struct {
+	needService bool
+	timeStamp   time
+}
+
+type hallCallsType 	[config.NumFloors][2]Call
+type cabCallsType 	[config.NumFloors]Call
+
+type hallBoolsType 	[config.NumFloors][2]bool
+type cabBoolsType 	[config.NumFloors]bool
+
+
+type hardwareCallsType struct {
+	hallCalls hallCallsType
+	cabCalls cabCallsType
+}
+
+
+type syncedDataType sturct {
+	hallBools hallBoolsType
+	cabBools cabBoolsType
+}
+
+
+func sync (hardwareCalls chan hardwareCallsType, syncedData chan syncedDataType) {
+	var localCabCalls cabCallsType
+	var localHallCalls hallCallsType
+
+	for {
+		select{
+		case incomingHardwareCalls := <-hardwareCalls:
+			
+		}
+	}
+}

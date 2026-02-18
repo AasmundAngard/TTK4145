be6bb0feffe242e94e0ab7af700242b89c6e07fb
AasmundAngard <wefling@live.no>
2026-02-18 12:24:52 +0100

    some work: pseudocode and flow

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "time"
 
 type Behaviour int
 
@@ -14,23 +12,60 @@ const (
 
 type Call struct {
 	needService bool
-	timeStamp   time
+	timeStamp   time.Duration
 }
 
 type ElevState struct {
 	behaviour   Behaviour
 	floor       int
 	direction   MotorDirection
-	cabRequests []bool
-	timeStamp   time //?
+}
+cabRequests 	[]bool // egen greie
+
+type ElevMovement struct {
+	behaviour 	Behaviour
+	direction 	MotorDirection
+}
+type AllElevatorsState struct {
+	hallCalls 	[]bool
+	cabCalls 	[][]bool // egen elevator som første entry
+	elevState 	[]ElevState
 }
 
+
 func main() {
-	fmt.Println("Hello TTK4145")
-	fmt.Println(elevio.pollRate)
 	// Init Hardware
+
+	elevio.Init("localhost:15657", 3) // Dette er til den lokale heisserveren man kan kjøre (alt. hardware)
+
+	drv_buttons := make(chan elevio.ButtonEvent)
+	go elevio.PollButtons(drv_buttons)
+	
+	// Polle stop-knapp og obstruction?
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
+
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+	
+	drv_floors := make(chan int)
+
+	go elevio.PollFloorSensor(drv_floors)
 	// Init sync -> Sync starts reading from the network and hardware
-	// Sync gives info to main about floor sensors
+	// Lag kanaler som skal passe info fra main
+
+	// Sync <-> main
+	// sync -> main: alle heistilstander og hall calls
+	// sync <- main: direction and movement
+	// sync <- main: completed calls
+
+	callsC := make(chan )  // From sync
+	localStateC := make(chan ElevState)   // To sync
+	completedCall := make(chan ButtonEvent) // To sync
+
+	go sync(callsC, currStateC, completedCall)
+
+	// main can poll floorsensor directly
 
 	// Sync should not broadcast before main says so? Maybe uninitialized tag?
 
@@ -45,19 +80,49 @@ func main() {
 
 	// Main starts receiving common hall calls,
 	// Main enters main loop
+	for {
 
-	// Main only receives common hall calls from sync
-	// Light up hall buttons based on hall calls
-	// Light up cab call buttons based on cab calls
+		// Main only receives common hall calls from sync
+		// Light up hall buttons based on hall calls
+		// Light up cab call buttons based on cab calls
 
-	// Main calls sequenceAsigner, assigning current calls and finding direction
+		// Main calls sequenceAsigner, assigning current calls and finding direction
 
-	// Main decides direction based on current state (doorOpen and result from sequenceAssigner)
+		// Main decides direction based on current state (doorOpen and result from sequenceAssigner)
 
-	// If stopButton -> stop
-	// If call at current floor -> stop
-	// If shouldMove -> Start actuators
-	// If shouldOpenDoor -> start door closing countdown
-	// if doorOpen and doorCountdown over -> try closing door
+		// Maybe make decisions based on channels and select case
+		// If moving:
+		// case a:=<-floorsensor:
+		//		if ain hallrequests or a in cabRequests
+		//			stopMotor
+		// 			direction blir hallrequests retning eller assigner retning
+		//
+		// case a:=<-stopButton:
+		// 		stopMotor
+	// 			direction blir samme som før
+		// else if doorOpen:
+		//	case a:=<-timer:
+		// 		if !obstruction && newdirection==olddirection
+		// 			close door
+		// 		else if obstruction
+		// 			opendoor
+		// 		else if newdirection != olddirection
+		// 			opendoor
+		// 			olddirection = newdirection
+		// 	else if doorClosed && idle		
+		// case 
+
+
+		// Open door
+		// Start timer (select case?)
+		// After timer, check door sensor
+
+		// If stopButton -> stop
+		// If call at current floor -> stop
+		// If shouldMove -> Start actuators
+		// If shouldOpenDoor -> start door closing countdown
+		// if doorOpen and doorCountdown over -> try closing door
+
+	}
 
 }

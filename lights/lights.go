4c69bc2d99df4fa3a8fbcb8598245047d9f2f9b2
AasmundAngard <wefling@live.no>
2026-03-16 13:15:39 +0100

    fixed lights

--- lights/lights.go
+++ lights/lights.go
@@ -7,15 +7,13 @@ import (
 )
 
 func SetLights(callsToLightsC <-chan elevsync.CommonCalls) {
-	select {
-	case calls := <-callsToLightsC:
+	for {
+		calls := <-callsToLightsC
 		for floor := 0; floor < config.NumFloors; floor++ {
 			elevio.SetButtonLamp(elevio.BT_HallUp, floor, calls.HallCalls[floor][0])
 			elevio.SetButtonLamp(elevio.BT_HallDown, floor, calls.HallCalls[floor][1])
 			elevio.SetButtonLamp(elevio.BT_Cab, floor, calls.CabCalls[floor])
 		}
-	default:
-
 	}
 
 }

78c27a3d2a9b3fe4f65350125c5fbd289fcea53c
krosten <Krisost@outlook.com>
2026-03-17 19:36:33 +0100

    changed config

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ const (
 	MotorTimeoutTime  = 5 * time.Second
 	DisconnectTime    = 1 * time.Second
 	DoorOpenTime      = 3 * time.Second
-	BroadcastTime     = 10 * time.Millisecond
+	BroadcastTime     = 25 * time.Millisecond
 	InitTimeout       = 2 * time.Second
 	InitRetryInterval = 400 * time.Millisecond
 	//SyncTimeout       = 1 * time.Second
